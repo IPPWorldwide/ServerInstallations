7b305a1df02e27418667da938a9c0b9ee13f9797
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-27 00:21:21 +0200

    Update install.sh

--- Debian/11/install.sh
+++ Debian/11/install.sh
@@ -1,3 +1,11 @@
 apt-get update
 apt-get -y upgrade
+apt-get -y install ca-certificates apt-transport-https software-properties-common -y
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list
+wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
+apt-get update -y
 apt-get -y install apache2 libapache2-mod-php8.0 php8.0
+apt-get -y install php php-common php-xml php-gd php-mbstring php-tokenizer php-json php-bcmath php-zip -y
+systemctl restart apache2
+rm -rf /var/www/html/index.html
+git clone https://github.com/IPPWorldwide/MerchantPortal.git /var/www/html/
