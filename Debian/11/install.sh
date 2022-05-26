1694f8e02ed5bf71914684d013d931bdf2e1b537
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-27 00:36:50 +0200

    Update install.sh

--- Debian/11/install.sh
+++ Debian/11/install.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install ca-certificates apt-transport-https software-properties-common -y
 echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/sury-php.list
-wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 apt-get update -y
 apt-get -y install apache2 libapache2-mod-php8.0 php8.0
 apt-get -y install php php-common php-xml php-gd php-mbstring php-tokenizer php-json php-bcmath php-zip -y
