05950adb076cfb6ecd863b56a74e9fb6e4e3a356
Mathias Gajhede <mag@venturecapitals.dk>
2022-05-27 23:58:48 +0200

    Update install.sh

--- Debian/11/install.sh
+++ Debian/11/install.sh
@@ -5,7 +5,7 @@ echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt
 wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 apt-get update -y
 apt-get -y install apache2 libapache2-mod-php8.0 php8.0
-apt-get -y install php php-common php-xml php-gd php-mbstring php-tokenizer php-json php-bcmath php-zip -y
+apt-get -y install php php-common php-xml php-gd php-mbstring php-tokenizer php-json php-bcmath php-zip php-curl -y
 systemctl restart apache2
 rm -rf /var/www/html/index.html
 git clone https://github.com/IPPWorldwide/MerchantPortal.git /var/www/html/
